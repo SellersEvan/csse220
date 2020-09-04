cf02185f1c90f960c3b6e34e73a4efce0912a494
Evan Sellers <sellers.evan@hype-industries.com>
2020-09-04 10:00:38 -0400

    HW1 done

--- Homework/HW1/src/HW1.java
+++ Homework/HW1/src/HW1.java
@@ -16,7 +16,7 @@ public class HW1 {
 	 * addFraction(1,2,1,4) returns 0.75
 	 */
 	public static double addFraction(int num1, int den1, int num2, int den2) {
-		return 0;
+		return ( ( double )( num1 * den2 + num2 * den1 ) / ( den1 * den2 ) );
 	}
 	
 	/**
@@ -38,8 +38,22 @@ public class HW1 {
 	 * @return a score
 	 */
 	public static int footballScore(String input) {
-
-		return 0;
+		return ( countStringPresent( input, "T" ) * 7 + countStringPresent( input, "F" ) * 3 );
 	}
 	
+
+	/** Count Times String Present (PUBLIC)
+	 * This will look in a string and return an
+	 * int of how many times another string appears
+	 * in the string.
+	 * 
+	 * @param input { string } search string
+	 * @param query { string } query buffer
+	 * @return { int } total number of times a string appears
+	 * 
+	 */
+	public static int countStringPresent( String input, String query ) {
+		return ( input.length() - input.replaceAll( query, "" ).length() );
+	}
+
 }//end class HW1
